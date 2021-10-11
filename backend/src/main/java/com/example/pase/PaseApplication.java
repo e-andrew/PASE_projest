1ae6749c104fbaf1d57f21a37f181fcd48afac73
Oleksii Krasnovskyi <finchatticu33@gmail.com>
2021-10-11 08:55:48 +0300

    add backend project structure

---
+++ backend/src/main/java/com/example/pase/PaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.pase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pase/PaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
